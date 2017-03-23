7de8f8d716d0c0ca4e1e96b0647e9eeb5cee959e
David Kopal <kopaldvd@gmail.com>
2017-03-23 13:55:20 +0100

    fix: indentation

--- src/index.js
+++ src/index.js
@@ -10,25 +10,25 @@ import { arrayify, clientOnly } from './utils'
 import Tip from './tip'
 
 const be = (moduleName, elementName, modifiers = []) => {
-    let className = elementName ? `${moduleName}-${elementName}` : `${moduleName}`;
+  let className = elementName ? `${moduleName}-${elementName}` : `${moduleName}`;
 
-    if (modifiers.length) {
-        className = modifiers.filter(x => x).reduce((acc, modifier) => `${acc} ${className}--${modifier}`, className);
-    }
+  if (modifiers.length) {
+    className = modifiers.filter(x => x).reduce((acc, modifier) => `${acc} ${className}--${modifier}`, className);
+  }
 
-    return className;
+  return className;
 }
 
 const toArray = value => {
-    if (Array.isArray(value)) {
-        return value;
-    }
-
-    if (typeof value === 'string') {
-        return value.split(' ');
-    }
-
+  if (Array.isArray(value)) {
     return value;
+  }
+
+  if (typeof value === 'string') {
+    return value.split(' ');
+  }
+
+  return value;
 }
 
 const supportedCSSValue = clientOnly(cssVendor.supportedValue)
@@ -131,14 +131,14 @@ const Popover = createClass({
   },
   componentWillUnmount () {
     /* If the Popover was never opened then then tracking
-    initialization never took place and so calling untrack
-    would be an error. Also see issue 55. */
+     initialization never took place and so calling untrack
+     would be an error. Also see issue 55. */
     if (this.hasTracked) this.untrackPopover()
   },
   resolvePopoverLayout () {
 
     /* Find the optimal zone to position self. Measure the size of each zone and use the one with
-    the greatest area. */
+     the greatest area. */
 
     const pickerSettings = {
       preferPlace: this.props.preferPlace,
@@ -146,18 +146,18 @@ const Popover = createClass({
     }
 
     /* This is a kludge that solves a general problem very specifically for Popover.
-    The problem is subtle. When Popover positioning changes such that it resolves at
-    a different orientation, its Size will change because the Tip will toggle between
-    extending Height or Width. The general problem of course is that calculating
-    zone positioning based on current size is non-trivial if the Size can change once
-    resolved to a different zone. Infinite recursion can be triggered as we noted here:
-    https://github.com/littlebits/react-popover/issues/18. As an example of how this
-    could happen in another way: Imagine the user changes the CSS styling of the popover
-    based on whether it was `row` or `column` flow. TODO: Find a solution to generally
-    solve this problem so that the user is free to change the Popover styles in any
-    way at any time for any arbitrary trigger. There may be value in investigating the
-    http://overconstrained.io community for its general layout system via the
-    constraint-solver Cassowary. */
+     The problem is subtle. When Popover positioning changes such that it resolves at
+     a different orientation, its Size will change because the Tip will toggle between
+     extending Height or Width. The general problem of course is that calculating
+     zone positioning based on current size is non-trivial if the Size can change once
+     resolved to a different zone. Infinite recursion can be triggered as we noted here:
+     https://github.com/littlebits/react-popover/issues/18. As an example of how this
+     could happen in another way: Imagine the user changes the CSS styling of the popover
+     based on whether it was `row` or `column` flow. TODO: Find a solution to generally
+     solve this problem so that the user is free to change the Popover styles in any
+     way at any time for any arbitrary trigger. There may be value in investigating the
+     http://overconstrained.io community for its general layout system via the
+     constraint-solver Cassowary. */
     if (this.zone) this.size[this.zone.flow === `row` ? `h` : `w`] += this.props.tipSize
     const zone = Layout.pickZone(pickerSettings, this.frameBounds, this.targetBounds, this.size)
     if (this.zone) this.size[this.zone.flow === `row` ? `h` : `w`] -= this.props.tipSize
@@ -177,8 +177,8 @@ const Popover = createClass({
     scrollSize.cross = scrollSize[axis.cross.size]
 
     /* When positioning self on the cross-axis do not exceed frame bounds. The strategy to achieve
-    this is thus: First position cross-axis self to the cross-axis-center of the the target. Then,
-    offset self by the amount that self is past the boundaries of frame. */
+     this is thus: First position cross-axis self to the cross-axis-center of the the target. Then,
+     offset self by the amount that self is past the boundaries of frame. */
     const pos = Layout.calcRelPos(zone, tb, this.size)
 
     /* Offset allows users to control the distance betweent the tip and the target. */
@@ -188,7 +188,7 @@ const Popover = createClass({
 
 
     /* Constrain containerEl Position within frameEl. Try not to penetrate a visually-pleasing buffer from
-    frameEl. `frameBuffer` length is based on tipSize and its offset. */
+     frameEl. `frameBuffer` length is based on tipSize and its offset. */
 
     const frameBuffer = this.props.tipSize + this.props.offset
     const hangingBufferLength = (dockingEdgeBufferLength * 2) + (this.props.tipSize * 2) + frameBuffer
@@ -202,18 +202,18 @@ const Popover = createClass({
     const popoverCrossEnd = pos[axis.cross.end]
 
     /* If the popover dose not fit into frameCrossLength then just position it to the `frameCrossStart`.
-    popoverCrossLength` will now be forced to overflow into the `Frame` */
+     popoverCrossLength` will now be forced to overflow into the `Frame` */
     if (pos.crossLength > frameCrossLength) {
       pos[axis.cross.start] = 0
 
-    /* If the `popoverCrossStart` is forced beyond some threshold of `targetCrossLength` then bound
-    it (`popoverCrossStart`). */
+      /* If the `popoverCrossStart` is forced beyond some threshold of `targetCrossLength` then bound
+       it (`popoverCrossStart`). */
 
     } else if (tb[axis.cross.end] < hangingBufferLength) {
       pos[axis.cross.start] = tb[axis.cross.end] - hangingBufferLength
 
-    /* If the `popoverCrossStart` does not fit within the inner frame (honouring buffers) then
-    just center the popover in the remaining `frameCrossLength`. */
+      /* If the `popoverCrossStart` does not fit within the inner frame (honouring buffers) then
+       just center the popover in the remaining `frameCrossLength`. */
 
     } else if (pos.crossLength > frameCrossInnerLength) {
       pos[axis.cross.start] = (frameCrossLength - pos.crossLength) / 2
@@ -226,15 +226,15 @@ const Popover = createClass({
     }
 
     /* So far the link position has been calculated relative to the target. To calculate the absolute
-    position we need to factor the `Frame``s scroll position */
+     position we need to factor the `Frame``s scroll position */
 
     pos[axis.cross.start] += scrollSize.cross
     pos[axis.main.start] += scrollSize.main
 
     /* Apply `flow` and `order` styles. This can impact subsequent measurements of height and width
-    of the container. When tip changes orientation position due to changes from/to `row`/`column`
-    width`/`height` will be impacted. Our layout monitoring will catch these cases and automatically
-    recalculate layout. */
+     of the container. When tip changes orientation position due to changes from/to `row`/`column`
+     width`/`height` will be impacted. Our layout monitoring will catch these cases and automatically
+     recalculate layout. */
     this.containerEl.style.flexFlow = zone.flow
     this.containerEl.style[jsprefix(`FlexFlow`)] = this.containerEl.style.flexFlow
     this.bodyEl.style.order = zone.order
@@ -242,18 +242,18 @@ const Popover = createClass({
 
     /* Apply Absolute Positioning. */
 
-	  if (this.props.parent) {
-			const parentOffset = this.props.parent.getBoundingClientRect()
-			this.bodyEl.style.top = `${pos.y}px`
-			this.bodyEl.style.left = `${pos.x}px`
-			this.containerEl.style.width = `${parentOffset.width}px`
-			this.containerEl.style.marginTop = `${parentOffset.height}px`
-			this.containerEl.style.zIndex = 1
-		}
-		else {
+    if (this.props.parent) {
+      const parentOffset = this.props.parent.getBoundingClientRect()
+      this.bodyEl.style.top = `${pos.y}px`
+      this.bodyEl.style.left = `${pos.x}px`
+      this.containerEl.style.width = `${parentOffset.width}px`
+      this.containerEl.style.marginTop = `${parentOffset.height}px`
+      this.containerEl.style.zIndex = 1
+    }
+    else {
       this.containerEl.style.top = `${pos.y}px`
       this.containerEl.style.left = `${pos.x}px`
-		}
+    }
   },
   checkTargetReposition () {
     if (this.measureTargetBounds()) this.resolvePopoverLayout()
@@ -334,23 +334,23 @@ const Popover = createClass({
     this.bodyEl = this.containerEl.querySelector(`.${be(className, 'body')}`)
 
     /* Note: frame is hardcoded to window now but we think it will
-    be a nice feature in the future to allow other frames to be used
-    such as local elements that further constrain the popover`s world. */
+     be a nice feature in the future to allow other frames to be used
+     such as local elements that further constrain the popover`s world. */
 
     this.frameEl = this.props.parent || window
     this.hasTracked = true
 
     /* Set a general interval for checking if target position changed. There is no way
-    to know this information without polling. */
+     to know this information without polling. */
     if (this.props.refreshIntervalMs) {
       this.checkLayoutInterval = setInterval(this.checkTargetReposition, this.props.refreshIntervalMs)
     }
 
     /* Watch for boundary changes in all deps, and when one of them changes, recalculate layout.
-    This layout monitoring must be bound immediately because a layout recalculation can recursively
-    cause a change in boundaries. So if we did a one-time force-layout before watching boundaries
-    our final position calculations could be wrong. See comments in resolver function for details
-    about which parts can trigger recursive recalculation. */
+     This layout monitoring must be bound immediately because a layout recalculation can recursively
+     cause a change in boundaries. So if we did a one-time force-layout before watching boundaries
+     our final position calculations could be wrong. See comments in resolver function for details
+     about which parts can trigger recursive recalculation. */
 
     this.onFrameScroll = throttle(this.onFrameScroll, minScrollRefreshIntervalMs)
     this.onFrameResize = throttle(this.onFrameResize, minResizeRefreshIntervalMs)
@@ -363,7 +363,7 @@ const Popover = createClass({
     resizeEvent.on(this.targetEl, this.onTargetResize)
 
     /* Track user actions on the page. Anything that occurs _outside_ the Popover boundaries
-    should close the Popover. */
+     should close the Popover. */
 
     window.addEventListener(`mousedown`, this.checkForOuterAction)
     window.addEventListener(`touchstart`, this.checkForOuterAction)
@@ -385,8 +385,8 @@ const Popover = createClass({
   untrackPopover () {
     clearInterval(this.checkLayoutInterval)
     this.frameEl.removeEventListener(`scroll`, this.onFrameScroll)
-		this.props.parent && (document.querySelector('.AutocompleteField-body').style.display = 'none')
-		resizeEvent.off(this.frameEl, this.onFrameResize)
+    this.props.parent && (document.querySelector('.AutocompleteField-body').style.display = 'none')
+    resizeEvent.off(this.frameEl, this.onFrameResize)
     resizeEvent.off(this.containerEl, this.onPopoverResize)
     resizeEvent.off(this.targetEl, this.onTargetResize)
     window.removeEventListener(`mousedown`, this.checkForOuterAction)
@@ -424,9 +424,9 @@ const Popover = createClass({
     }
 
     /* If we pass array of nodes to component children React will complain that each
-    item should have a key prop. This is not a valid requirement in our case. Users
-    should be able to give an array of elements applied as if they were just normal
-    children of the body component (note solution is to spread array items as args). */
+     item should have a key prop. This is not a valid requirement in our case. Users
+     should be able to give an array of elements applied as if they were just normal
+     children of the body component (note solution is to spread array items as args). */
 
     const popoverBody = arrayify(this.props.body)
 
